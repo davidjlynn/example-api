38a7487fb7bd30dc642bb49ca0dfcb8087e3aee4
David James Lynn <david.james.lynn@gmail.com>
2025-05-14 19:42:16 +0100

    A start to the repository

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  alias(libs.plugins.versions)
+  alias(libs.plugins.spotless)
+  alias(libs.plugins.openapiGenerator)
+}
+
+group = "com.davidjlynn"
+
+repositories { mavenCentral() }
+
+openApiValidate { inputSpec.set("src/main/openapi/openapi.yaml") }
+
+spotless {
+  format("misc") {
+    target(".gitattributes", ".gitignore")
+    trimTrailingWhitespace()
+    leadingTabsToSpaces(2)
+    endWithNewline()
+  }
+  yaml {
+    target("src/**/*.yaml")
+    jackson()
+      .yamlFeature("MINIMIZE_QUOTES", true)
+      .yamlFeature("WRITE_DOC_START_MARKER", false)
+      .yamlFeature("INDENT_ARRAYS_WITH_INDICATOR", true)
+  }
+  kotlinGradle { ktfmt().googleStyle() }
+}
